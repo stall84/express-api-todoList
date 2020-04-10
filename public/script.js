b2b3b21dad77f7a2dcbc9e0c30b83e96c137c043
Michael Stallings <stall84@gmail.com>
2020-04-10 19:59:41 -0400

    large changes made to index.html to remove form element (conflict) and set id for todo-content to be displayed. In script.js we have a working version where the todo that's entered is rendered into the display table.. however the next todo erases it and replaces it. working on functions to fix

--- public/script.js
+++ public/script.js
@@ -1,20 +1,37 @@
 // JS for index.html
 
-document.getElementById('submitButton').addEventListener("click", submit());
+document.getElementById('todoForm').addEventListener("submit", submit);
 
-function submit () {
+function submit (event) {
+    event.preventDefault();
+    console.log(event);
+    let time = event.target.elements.inputTime.value
+    let todo = event.target.elements.inputTodo.value
+    const todoObject = {time,todo}
 
-let inputTime = document.getElementById('inputTime');
-let inputTodo = document.getElementById('inputTodo');
-console.log(`Initial constructed data is inputTime: ${inputTime}, inputTodo: ${inputTodo}`);
+    fetch('/api/todos', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todoObject)
+    }).then(response => response.json()).then((data) =>{
+        console.log(data)
+        const todoDisplay = document.getElementById('todo-content');
+        todoDisplay.innerHTML = `
+        <tr>
+            <th scope="row">${data.time}</th>
+            <td>${data.todo}</td>
+        </tr>
+        `
+       
+        // Create a render function to add the todo to the DOM
 
-    axios.post('/api/todos', {
-        time: inputTime,
-        todo: inputTodo
-    })
-       .then((response) => {
-    console.log(`Success! ${response.data}`);
-    },       (error) => {
-    console.log(`There was an error in response ${error}`);
-});
+    }).catch(e => console.log(`Error is ${e}`))
+    
+}
+
+// function to render the individual single todo that's entered and returns
+function renderToDo () {
+
 }

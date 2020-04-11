aa68ebceb36816e13cbca0df9380db171b4f6f74
Michael Stallings <stall84@gmail.com>
2020-04-10 21:37:41 -0400

    Successfully created a rendering function in script.js feeding it the response todo array from the server, rendering it to the page. Added numerous comments in code. All thats left is fine-tuning the appearance of the html page

--- public/script.js
+++ public/script.js
@@ -28,9 +28,34 @@ function submit (event) {
     axios.get('/api/todos') 
         .then(response => {
             console.log(response.data);
-            //var todosArray = response.data;
-            //const fullTodoDisplay = document.getElementById('full-todo-content');
+            // create/define the array that will store the response array of objects from the servers' global todo variable
+            var todosArray = response.data;
+            const fullTodoDisplay = document.getElementById('full-todo-content');
+            // finally assign the inner html of the 'grabbed' full-todo form to the render function taking the todos array as argument/input
+            fullTodoDisplay.innerHTML = renderTodos(todosArray);
         
         }).catch(e => console.log(`Error occurred: ${e}`));
 };
 
+    // this render function will take the input argument of the response array from the axios get call directly above
+    // the response array/object returned in the get call above is the full todos array being returned from the server
+    // after being updated with the new todo made in the POST request further above
+function renderTodos (todos) {
+    // Create an 'output array' to hold each individually rendered todo coming in from the global todo array on the server
+    var outputArray = [];
+    // Using a for-loop for simplicity, could have used an array method. Will iterate through each individual array object contained in the todo array from server
+    // and will render it in html, then it will push that html template-literal into the output array
+    for (let i = 0; i < todos.length; i++) {
+        let currTodo = todos[i];
+        let todosHTML = `
+            <tr>
+                <th scope="row">${currTodo.time}</th>
+                <td>${currTodo.todo}</td>
+            </tr>
+        `
+        outputArray.push(todosHTML);
+    }
+    // must return the joined output array to yield the value of the function. joining removes the commas otherwise present in objects
+    return outputArray.join('');
+
+};
